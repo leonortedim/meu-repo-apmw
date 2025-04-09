aad9d479acb98b2bf4aa76d9f00a88546c367f69
leonortedim <leonortedim@gmail.com>
2025-04-09 13:58:06 +0100

    bugou

---
+++ exercicio-js/JavaScript.js
@@ -0,0 +1,20 @@
+let eu = "Leonor";
+let idade = 19;
+
+let idadex = idade * 7;
+
+console.log(idadex.toString(), "is my age multiplied by 7");
+
+let amigos = ["Maria", "José", "Ana"];
+
+let objeto = {
+  nome: "Leonor",
+  idade: 19,
+  isStudent: true,
+};
+
+let amigoscoisos = [
+  { nome: "Maria", idade: 20, isStudent: true },
+    { nome: "José", idade: 22, isStudent: false },
+    { nome: "Ana", idade: 21, isStudent: true },
+]
