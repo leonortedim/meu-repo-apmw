6982720ff1f766d44e261baa21fb7cee870b146c
leonortedim <leonortedim@gmail.com>
2025-04-24 12:52:39 +0100

    svelte work

--- Svelte/src/routes/+page.svelte
+++ Svelte/src/routes/+page.svelte
@@ -1,17 +1,19 @@
 <script>
-    let light = src=("https://i.postimg.cc/KjK1wL3c/bulb-off.png");
-    let dark = src=("https://i.postimg.cc/3x0JYq2g/bulb-on.png");
-    let lightOn = lightOff;
+    import { get } from "svelte/store";
+
+    let dark = 'https://i.postimg.cc/KjK1wL3c/bulb-off.png';
+    let light = 'https://i.postimg.cc/6QyTynzr/bulb-on.png';
+    let bulb = dark;
     let bgColor = 'bg-black';
     let buttonTxt = 'Turn On';
 
     function toggleLight() {
-        if (lightOn === lightOff) {
-            lightOn = light;
+        if (bulb === dark) {
+            bulb = light;
             bgColor = 'bg-white transition duration-500 ease-in-out';
             buttonTxt = 'Turn Off';
         } else {
-            lightOn = dark;
+            bulb = dark;
             bgColor = 'bg-black transition duration-500 ease-in-out';
             buttonTxt = 'Turn On';
         }
@@ -19,9 +21,14 @@
 </script>
 
 <div class="{bgColor} items-center justify-center gap-4 flex flex-col">
-    <img class="items-center mx-auto" src={light} alt="lightbulb"/>
+    <img class="items-center mx-auto" src={bulb} alt="lightbulb"/>
     <button on:click={toggleLight} class="bg-amber-400 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded">
         {buttonTxt}</button>
 </div>
+
+
+<style>
+
+</style>
     
 
